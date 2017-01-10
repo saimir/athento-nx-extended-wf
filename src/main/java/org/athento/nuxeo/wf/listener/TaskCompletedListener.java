0127144e1652a0f79e6d75aa88ba5951a07a2e41
vs_ <vs@athento.com>
2017-01-10 22:26:14 +0100

    Execute transitions from nodes with hasTask=false

--- src/main/java/org/athento/nuxeo/wf/listener/TaskCompletedListener.java
+++ src/main/java/org/athento/nuxeo/wf/listener/TaskCompletedListener.java
@@ -8,6 +8,11 @@ import org.nuxeo.ecm.core.api.*;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.platform.relations.api.Graph;
+import org.nuxeo.ecm.platform.routing.api.DocumentRoutingConstants;
+import org.nuxeo.ecm.platform.routing.core.impl.GraphNode;
+import org.nuxeo.ecm.platform.routing.core.impl.GraphRoute;
+import org.nuxeo.ecm.platform.routing.core.impl.GraphRouteImpl;
 import org.nuxeo.ecm.platform.task.Task;
 import org.nuxeo.runtime.api.Framework;
 
@@ -24,6 +29,9 @@ public class TaskCompletedListener implements EventListener {
     private static final Log LOG = LogFactory
             .getLog(TaskCompletedListener.class);
 
+    /** Last transition execution control. */
+    private static String lastExecutedTransition = null;
+
     /**
      * Handle notification.
      *
@@ -31,10 +39,41 @@ public class TaskCompletedListener implements EventListener {
      * @throws Exception
      */
     @Override
-    public void handleEvent(Event event) throws ClientException {
+    public void handleEvent(Event event) {
         if (event != null) {
-            if (event.getContext() instanceof DocumentEventContext) {
-                CoreSession session = event.getContext().getCoreSession();
+            CoreSession session = event.getContext().getCoreSession();
+            if (event.getName().equals(DocumentRoutingConstants.Events.beforeRouteStart.name())) {
+                // Start task
+                GraphRouteImpl startRoute = (GraphRouteImpl) event.getContext().getProperty("documentElementEventContextKey");
+                GraphNode startNode = startRoute.getStartNode();
+                List<GraphNode.Transition> nodeTransitions = startNode.getOutputTransitions();
+                if (nodeTransitions.size() > 0) {
+                    String transitionExecuted = nodeTransitions.get(0).getId();
+                    if (!startRoute.getAttachedDocumentModels().isEmpty()) {
+                        executeTransition(session, startRoute.getAttachedDocumentModels().get(0), startNode, transitionExecuted);
+                    }
+                }
+            } else if (event.getName().equals("documentModified")) {
+                // Check transition after node document modification
+                DocumentModel autoNode = ((DocumentEventContext) event.getContext()).getSourceDocument();
+                if (autoNode.getType().equals("RouteNode")) {
+                    GraphNode node = autoNode.getAdapter(GraphNode.class);
+                    if (!node.hasTask()) {
+                        DocumentModel startRouteDoc = session.getDocument(autoNode.getParentRef());
+                        if (startRouteDoc != null) {
+                            GraphRoute startRoute = startRouteDoc.getAdapter(GraphRoute.class);
+                            // Check transitions
+                            List<GraphNode.Transition> nodeTransitions = node.getOutputTransitions();
+                            if (nodeTransitions.size() > 0) {
+                                String transitionExecuted = nodeTransitions.get(0).getId();
+                                if (!startRoute.getAttachedDocumentModels().isEmpty()) {
+                                    executeTransition(session, startRoute.getAttachedDocumentModels().get(0), node, transitionExecuted);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
                 // Add task property from event document
                 DocumentModel document = ((DocumentEventContext) event.getContext()).getSourceDocument();
                 if (document != null) {
@@ -43,43 +82,68 @@ public class TaskCompletedListener implements EventListener {
                         DocumentModel taskNodeDoc = WorkflowUtils.getDocumentNodeFromTask(session, task);
                         // Check transitions
                         if (taskNodeDoc != null) {
-                            LOG.info("Task node doc to check transitions " + taskNodeDoc.getId());
-                            String nodeId = (String) taskNodeDoc.getPropertyValue("rnode:nodeId");
-                            if (taskNodeDoc.hasSchema("var-" + nodeId)) {
-                                try {
-                                    String transitionsData = (String) taskNodeDoc.getPropertyValue("var-" + nodeId + ":lfTransitions");
-                                    LOG.info("Transitions " + transitionsData);
-                                    if (transitionsData != null) {
-                                        DocumentModel targetDoc = session.getDocument(new IdRef(task.getTargetDocumentId()));
-                                        DocumentModel nextTaskDocument = WorkflowUtils.getTaskDocument(session, targetDoc, "opened");
-                                        if (nextTaskDocument != null) {
-                                            Task nextTask = nextTaskDocument.getAdapter(Task.class);
-                                            DocumentModel nextNodeDocument = WorkflowUtils.getDocumentNodeFromTask(session, nextTask);
-                                            if (nextNodeDocument != null) {
-                                                String transitionExecuted = getLastTransitionExecuted(taskNodeDoc, (String) nextNodeDocument.getPropertyValue("rnode:nodeId"));
-                                                LOG.info("transitionExecuted" + transitionExecuted);
-                                                String[] transitions = transitionsData.split(";");
-                                                for (String transitionInfo : transitions) {
-                                                    String [] transitionData = transitionInfo.split(":");
-                                                    if (transitionData.length == 2) {
-                                                        String transitionName = transitionData[0];
-                                                        if (transitionExecuted.equals(transitionName)) {
-                                                            String transition = transitionData[1];
-                                                            if (transition != null && !"null".equals(transition)) {
-                                                                DocumentModel targetDocument = WorkflowUtils.getTargetDocumentFromTask(session, nextTask);
-                                                                session.followTransition(targetDocument, transition);
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                } catch (PropertyException e) {
-                                    LOG.info("Property lfTransitions is not found for taskNode " + taskNodeDoc.getId());
-                                }
-                            }
+                            LOG.info("Task node doc to check transitions " + taskNodeDoc.getId() + ", " + taskNodeDoc.getName());
+                            manageTransitions(session, task, taskNodeDoc);
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Manage lifecycle transitions.
+     *
+     * @param session
+     * @param task
+     * @param taskNodeDoc
+     */
+    private void manageTransitions(CoreSession session, Task task, DocumentModel taskNodeDoc) {
+        String nodeId = (String) taskNodeDoc.getPropertyValue("rnode:nodeId");
+        if (taskNodeDoc.hasSchema("var-" + nodeId)) {
+            try {
+                String transitionsData = (String) taskNodeDoc.getPropertyValue("var-" + nodeId + ":lfTransitions");
+                if (transitionsData != null) {
+                    DocumentModel targetDoc = session.getDocument(new IdRef(task.getTargetDocumentId()));
+                    DocumentModel nextTaskDocument = WorkflowUtils.getTaskDocument(session, targetDoc, "opened");
+                    if (nextTaskDocument != null) {
+                        Task nextTask = nextTaskDocument.getAdapter(Task.class);
+                        DocumentModel nextNodeDocument = WorkflowUtils.getDocumentNodeFromTask(session, nextTask);
+                        if (nextNodeDocument != null) {
+                            String transitionExecuted = getLastWorkflowTransitionExecuted(taskNodeDoc, (String) nextNodeDocument.getPropertyValue("rnode:nodeId"));
+                            GraphNode node = taskNodeDoc.getAdapter(GraphNode.class);
+                            executeTransition(session, WorkflowUtils.getTargetDocumentFromTask(session, nextTask), node, transitionExecuted);
+                        }
+                    }
+                }
+            } catch (PropertyException e) {
+                LOG.info("Property lfTransitions is not found for taskNode " + taskNodeDoc.getId());
+            }
+        }
+    }
+
+    /**
+     * Execute transition.
+     *
+     * @param session
+     * @param targetDocument
+     * @param node
+     * @param transitionExecuted
+     */
+    private void executeTransition(CoreSession session, DocumentModel targetDocument, GraphNode node, String transitionExecuted) {
+        String transitionsData = (String) node.getDocument().getPropertyValue("var-" + node.getId() + ":lfTransitions");
+        String[] transitions = transitionsData.split(";");
+        for (String transitionInfo : transitions) {
+            String[] transitionData = transitionInfo.split(":");
+            if (transitionData.length == 2) {
+                String transitionName = transitionData[0];
+                if (transitionExecuted.equals(transitionName)) {
+                    String transition = transitionData[1];
+                    if (transition != null && !"null".equals(transition)
+                            && !transition.equals(lastExecutedTransition)) {
+                        session.followTransition(targetDocument, transition);
+                        // Update lastExecutedTransition
+                        lastExecutedTransition = transition;
                     }
                 }
             }
@@ -93,7 +157,7 @@ public class TaskCompletedListener implements EventListener {
      * @param currentNodeId
      * @return
      */
-    private String getLastTransitionExecuted(DocumentModel lastNode, String currentNodeId) {
+    private String getLastWorkflowTransitionExecuted(DocumentModel lastNode, String currentNodeId) {
         if (lastNode == null) {
             return null;
         }
